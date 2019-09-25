6141c612c419342927de040d3b32300ec384db10
Luis Garreta - Agrosavia <lgarreta@agrosavia.co>
2019-09-25 10:52:00 -0500

    Before PCAs

---
+++ dev/agrosavia-gwaspoly.R
@@ -0,0 +1,50 @@
+library (GWASpoly)
+phenotypeFile = "agrosavia-phenotype-gwaspoly.tbl"
+genotypeFile  = "agrosavia-genotype-gwaspoly-checked.tbl"
+
+phex = read.table ("example/phenotype-TableS2.tbl", header=T, sep=",")
+gnex = read.table ("example/genotype-TableS1.tbl", header=T, sep=",")
+
+AnnShip = read.table ("other-files/potato_infinium_8303_map_context_DM_v3_superscaffolds.txt", header=T, sep="\t")
+AnnGeno = read.table ("other-files/potato_8303SNPs_potato_dm_v4.03.gff3", header=F, sep="\t")
+
+ph = read.table (phenotypeFile, header=T, sep=",")
+gn = read.table (genotypeFile, header=T, sep=",")
+
+
+# Read input genotype and genotype
+data = read.GWASpoly (ploidy = 4, delim=",", format = "numeric", n.traits = 1, 
+                      pheno.file = phenotypeFile, geno.file = genotypeFile)
+
+# Populations structure by kinship
+data2 <- set.K(data)
+
+# Used to include population structure covariates
+#params <- set.params(fixed=c("Grp1","Grp2","Grp3","Grp4"),
+#                     fixed.type=rep("numeric",4))
+params = NULL
+# GWAS execution
+data3 = GWASpoly(data2, models=c("general","additive","1-dom", 
+                                 "2-dom"),traits=c("gota"), params=params)
+# QQ-plot Output
+pdf (file="plots-qq-gwaspoly.pdf")
+  par(mfrow=c(2,3)) #specifies a 2 x 3 panel
+  models <- c("additive","general","1-dom-alt","1-dom-ref","2-dom-alt","2-dom-ref")
+  for (i in 1:6) {
+    qq.plot(data3,trait="gota",model=models[i])
+  }
+dev.off()
+
+# QTL Detection
+data4 = set.threshold (data3, method="Bonferroni",level=0.05)
+get.QTL (data4)
+
+# Manhattan plot Output
+pdf (file="plots-manhattan-gwaspoly.pdf")
+  par(mfrow=c(2,3)) #specifies a 1 x 3 panel
+  for (i in 1:6) {
+    manhattan.plot (data4, trait="gota", model=models [i])
+  }  
+dev.off ()
+
+
