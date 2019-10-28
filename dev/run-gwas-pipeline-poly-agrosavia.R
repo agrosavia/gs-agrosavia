a1be7f0a723f6361ee7b8d25a9f01f64dc190f36
Luis Garreta <lgarreta@uao.edu.co>
2019-10-28 09:57:30 -0500

    r2.1: Support for multiple command line arguments (dynamic)

--- dev/run-gwas-pipeline-poly-agrosavia.R
+++ dev/run-gwas-pipeline-poly-agrosavia.R
@@ -1,1 +1,13 @@
-gwas-pipeline-poly-agrosavia.R phenotype-checked.tbl genotype-checked-diplo.tbl structure-checked.tbl snps-annotations.tbl
+#!/usr/bin/Rscript
+library (parallel)
+
+models = c("Naive", "Structure", "Kinship", "Kinship+Structure")
+
+cmmList = list()
+for (mdl in models) {
+	cmm = sprintf ("gwas-polypipeline.R --pheno phenotype.tbl --geno genotype-diplo.tbl --struct structure.tbl --snps snps-annotations.tbl --ploidy 2 --model %s", mdl)
+	message (cmm)			   
+	cmmList = append (cmmList, cmm)
+}
+#mclapply (cmmList, system, mc.cores=3)
+sapply (cmmList, system)
