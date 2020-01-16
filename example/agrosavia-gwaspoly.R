7821b021f26bfeef4ff2e7efde5d3d4d7722f147
Luis Garreta - Agrosavia <lgarreta@agrosavia.co>
2020-01-16 10:10:21 -0500

    r3.0: Run code example gwaspoly with different genotype formats. Convertion problem from numeric to ACGT: not real ref/alt allleles

--- example/agrosavia-gwaspoly.R
+++ example/agrosavia-gwaspoly.R
@@ -1,6 +1,17 @@
 #!/usr/bin/Rscript
 library (GWASpoly)
 
+TRAIT="tuber_shape"
+
+#-------------------------------------------------------------
+# Add label to filename
+#-------------------------------------------------------------
+addLabel <- function (filename, label)  {
+	nameext = strsplit (filename, split="[.]")
+	newName = paste0 (nameext [[1]][1], "-", label, ".", nameext [[1]][2])
+	return (newName)
+}
+
 #-------------------------------------------------------------
 # Print a log message with the parameter
 #-------------------------------------------------------------
@@ -13,17 +24,8 @@ msg <- function (...) {
 args = commandArgs(trailingOnly = TRUE)
 print (args)
 
-phenotypeFile = args [1]
-genotypeFile  = args [2]
-
-#phenotypeFile = "agrosavia-phenotype-gwaspoly.tbl"
-#genotypeFile  = "agrosavia-genotype-gwaspoly-checked.tbl"
-
-#phex = read.table ("example/phenotype-TableS2.tbl", header=T, sep=",")
-#gnex = read.table ("example/genotype-TableS1.tbl", header=T, sep=",")
-
-#AnnShip = read.table ("other-files/potato_infinium_8303_map_context_DM_v3_superscaffolds.txt", header=T, sep="\t")
-#AnnGeno = read.table ("other-files/potato_8303SNPs_potato_dm_v4.03.gff3", header=F, sep="\t")
+genotypeFile  = args [1]
+phenotypeFile = args [2]
 
 ph = read.table (phenotypeFile, header=T, sep=",")
 gn = read.table (genotypeFile, header=T, sep=",")
@@ -36,37 +38,45 @@ data = read.GWASpoly (ploidy = 4, delim=",", format = "ACGT", n.traits = 1,
 
 message (">>> Calculating kinship...")
 # Populations structure by kinship
-data2 <- set.K(data)
+data2 <- set.K(data, K=NULL)
 
 # Used to include population structure covariates
 #params <- set.params(n.PC=10)
-#params <- set.params(fixed=c("Grp1","Grp2","Grp3","Grp4"),
+#params <- set.params(fixed=c("K1","K2","K3","K4"),
 #                     fixed.type=rep("numeric",4))
 params = NULL
 # GWAS execution
 message (">>> Running GWASpoly...")
 data3 = GWASpoly(data2, models=c("general","additive","1-dom", 
-                                 "2-dom"),traits=c("gota"))
+                                 "2-dom"),traits=c(TRAIT),n.core=4)
+
+# QTL Detection
+#data4 = set.threshold (data3, method="Bonferroni",level=0.05, n.core=4)
+data4 = set.threshold (data3, method="FDR",level=0.05, n.core=4)
+significativeQTLs = get.QTL (data4)
+outFile = paste0 ("out-QTL-", TRAIT,".scores")
+write.table (file=outFile, significativeQTLs, quote=F, sep="\t", row.names=F)
+
 # QQ-plot Output
 message ("Ploting results...")
-pdf (file="plots-qq-gwaspoly.pdf")
+outFile = paste0 ("out-QQ-", TRAIT,".pdf")
+pdf (file=outFile)
   par(mfrow=c(2,3)) #specifies a 2 x 3 panel
   models <- c("additive","general","1-dom-alt","1-dom-ref","2-dom-alt","2-dom-ref")
   for (i in 1:6) {
-    qq.plot(data3,trait="gota",model=models[i])
+    qq.plot(data3,trait=TRAIT,model=models[i])
   }
 dev.off()
 
-# QTL Detection
-data4 = set.threshold (data3, method="Bonferroni",level=0.05)
-get.QTL (data4)
 
 # Manhattan plot Output
-pdf (file="plots-manhattan-gwaspoly.pdf")
+outFile = paste0 ("out-manhattan-", TRAIT,".pdf")
+pdf (file=outFile)
   par(mfrow=c(2,3)) #specifies a 1 x 3 panel
   for (i in 1:6) {
-    manhattan.plot (data4, trait="gota", model=models [i])
+    manhattan.plot (data4, trait=TRAIT, model=models [i])
   }  
 dev.off ()
+
 
 
